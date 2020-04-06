b16f0b6f6ce023f7a71fbab72737fb398eda7bdf
Vinod Karivelil <vinodkarivelil@gmail.com>
2020-04-06 14:21:23 +0000

    First commit of conditionals section

---
+++ conditionals.js
@@ -0,0 +1,11 @@
+const temp = -1;
+
+if (temp < 0) {
+  console.log('Don\'t forget to pick out a scarf.');
+} else if (temp < 15) {
+  console.log('Pick out long sleeves.');
+} else {
+  console.log('Short sleeves are fine.');
+}
+
+console.log('Now you\'re ready to go outside.')
